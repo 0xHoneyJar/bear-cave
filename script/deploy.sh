39d3c07523990dbe683e41275d300200273abc59
soju <underrated@gmail.com>
2023-03-01 12:23:08 -0800

    fix: deployment script paths

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,5 @@
-source ../.env
+source .env
 
-forge script ../script/DeployTestnet.s.sol:DeployScript --rpc-url $RPC_URL --private-key $PRIVATE_KEY --slow -vvvvv \
+forge script script/DeployTestnet.s.sol:DeployScript --rpc-url $RPC_URL --private-key $PRIVATE_KEY --slow -vvvvv \
 --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY \ # uncomment to broadcast to the network
 # --resume # uncomment to resume from a previous deployment
