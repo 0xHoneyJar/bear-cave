efffe5b59bd193c2f3aea12b89dc8366166fc5f9
Beep Boop <gpt4.20@proton.me>
2023-04-19 01:38:07 -0700

    misc

--- script/TestScript.s.sol
+++ script/TestScript.s.sol
@@ -3,12 +3,20 @@ pragma solidity 0.8.17;
 
 import "./THJScriptBase.sol";
 
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {ERC1155} from "solmate/tokens/ERC1155.sol";
 import {HoneyBox} from "src/HoneyBox.sol";
 // Calls honeyBox.addBundle
 
 /// @notice this script is only meant to test
 contract TestScript is THJScriptBase {
     using stdJson for string;
+
+    address private honeyBox;
+
+    function setUp() public {
+        honeyBox = _readAddress("HONEYBOX_ADDRESS");
+    }
 
     function run(string calldata env) public override {
         string memory json = _getConfig(env);
@@ -17,7 +25,14 @@ contract TestScript is THJScriptBase {
         uint256[] memory tokenIds = json.readUintArray(".bundleTokens[*].id");
         bool[] memory isERC1155s = json.readBoolArray(".bundleTokens[*].isERC1155");
 
+        // Build out txns here.
         for (uint256 i = 0; i < addresses.length; i++) {
+            if (isERC1155s[i]) {
+                ERC1155(addresses[i]).setApprovalForAll(honeyBox, true);
+                continue;
+            }
+
+            ERC721(addresses[i]).approve(honeyBox, tokenIds[i]);
             console.log(addresses[i]);
             console.log(tokenIds[i]);
             console.log(isERC1155s[i]);
