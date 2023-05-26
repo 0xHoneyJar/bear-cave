9cb6625adcaef50ba58a5ac4ef7a3f4ea766c045
Beep Boop <gpt4.20@proton.me>
2023-05-26 00:36:07 -0700

    fix: decoupling chainId from lzChainId

--- script/TestScript.s.sol
+++ script/TestScript.s.sol
@@ -2,8 +2,17 @@
 pragma solidity 0.8.17;
 
 import {HibernationDen} from "src/HibernationDen.sol";
+import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
 
 import "./THJScriptBase.sol";
+
+contract MyContract {
+    uint16 private immutable _chainId;
+
+    constructor() {
+        _chainId = SafeCastLib.safeCastTo16(block.chainid);
+    }
+}
 
 /// @notice this script is only meant to test do not use for production
 contract TestScript is THJScriptBase("gen2") {
@@ -19,16 +28,17 @@ contract TestScript is THJScriptBase("gen2") {
         // vm.startBroadCast(gameAdmin); // Simulate with GameAdmin
         vm.startBroadcast();
 
-        HibernationDen den = new HibernationDen(
-            0x6D80646bEAdd07cE68cab36c27c626790bBcf17f,
-            0x0B9a7a17D0EBc02EF1832ea040Cb629eCa83AD14,
-            0x613a642C473D4DB6363Bd9c58c1ef101dDf40232,
-            0x9Db2ea779Cd3F9F8aEB5E58EB1223a585a4D7D68,
-            0x0B9a7a17D0EBc02EF1832ea040Cb629eCa83AD14,
-            0xF951bA8107D7BF63733188E64D7E07bD27b46Af7,
-            0xF951bA8107D7BF63733188E64D7E07bD27b46Af7,
-            223300000000000000
-        );
+        MyContract deployment = new MyContract();
+        // HibernationDen den = new HibernationDen(
+        //     0x6D80646bEAdd07cE68cab36c27c626790bBcf17f,
+        //     0x0B9a7a17D0EBc02EF1832ea040Cb629eCa83AD14,
+        //     0x613a642C473D4DB6363Bd9c58c1ef101dDf40232,
+        //     0x9Db2ea779Cd3F9F8aEB5E58EB1223a585a4D7D68,
+        //     0x0B9a7a17D0EBc02EF1832ea040Cb629eCa83AD14,
+        //     0xF951bA8107D7BF63733188E64D7E07bD27b46Af7,
+        //     0xF951bA8107D7BF63733188E64D7E07bD27b46Af7,
+        //     223300000000000000
+        // );
         vm.stopBroadcast();
     }
 }
--- src/CrossChainTHJ.sol
+++ src/CrossChainTHJ.sol
@@ -4,13 +4,13 @@ pragma solidity 0.8.17;
 import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
 
 abstract contract CrossChainTHJ {
-    uint16 private immutable _chainId;
+    uint256 private immutable _chainId;
 
-    function getChainId() internal view returns (uint16) {
+    function getChainId() internal view returns (uint256) {
         return _chainId;
     }
 
     constructor() {
-        _chainId = SafeCastLib.safeCastTo16(block.chainid);
+        _chainId = block.chainid;
     }
 }
