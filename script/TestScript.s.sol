f81858aa09c51b51c5df9150a1e67ab1296b3f00
Beep Boop <gpt4.20@proton.me>
2023-05-26 01:37:13 -0700

    deploying and updating scripts

--- script/TestScript.s.sol
+++ script/TestScript.s.sol
@@ -30,13 +30,13 @@ contract TestScript is THJScriptBase("gen2") {
 
         MyContract deployment = new MyContract();
         // HibernationDen den = new HibernationDen(
-        //     0x6D80646bEAdd07cE68cab36c27c626790bBcf17f,
-        //     0x0B9a7a17D0EBc02EF1832ea040Cb629eCa83AD14,
-        //     0x613a642C473D4DB6363Bd9c58c1ef101dDf40232,
-        //     0x9Db2ea779Cd3F9F8aEB5E58EB1223a585a4D7D68,
-        //     0x0B9a7a17D0EBc02EF1832ea040Cb629eCa83AD14,
-        //     0xF951bA8107D7BF63733188E64D7E07bD27b46Af7,
-        //     0xF951bA8107D7BF63733188E64D7E07bD27b46Af7,
+        // 0x6D80646bEAdd07cE68cab36c27c626790bBcf17f,
+        // 0x0B9a7a17D0EBc02EF1832ea040Cb629eCa83AD14,
+        // 0x613a642C473D4DB6363Bd9c58c1ef101dDf40232,
+        // 0x9Db2ea779Cd3F9F8aEB5E58EB1223a585a4D7D68,
+        // 0x0B9a7a17D0EBc02EF1832ea040Cb629eCa83AD14,
+        // 0xF951bA8107D7BF63733188E64D7E07bD27b46Af7,
+        // 0xF951bA8107D7BF63733188E64D7E07bD27b46Af7,
         //     223300000000000000
         // );
         vm.stopBroadcast();
