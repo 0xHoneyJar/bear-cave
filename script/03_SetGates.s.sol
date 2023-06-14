dbd2b7a830688958db1a61649b435a7c116b78b2
Beep Boop <gpt4.20@proton.me>
2023-06-14 12:55:22 -0700

    script: swapping gates

--- script/03_SetGates.s.sol
+++ script/03_SetGates.s.sol
@@ -22,8 +22,8 @@ contract SetGates is THJScriptBase("gen3") {
         // TODO: could be moved into config
         //     function addGate(uint256 bundleId, bytes32 root_, uint32 maxClaimable_, uint8 stageIndex_)
         gk.addGate(bundleId, 0x8caa438bc275ce2d266d67297054888df90c2cc2cd256bbe7a6f50ed605269c4, 214, 0); // IG
-        gk.addGate(bundleId, 0x093dce164993a0878f91817bd0363c68adaf8eb6ea72fa275ad644d050fa3a09, 1000, 0); // BG
-        gk.addGate(bundleId, 0xbb5c72e4fd398ac4b6647eb5746c12b695820935f228ecdd47375266a991f6d6, 1378, 0); // HG
+        gk.addGate(bundleId, 0xbb5c72e4fd398ac4b6647eb5746c12b695820935f228ecdd47375266a991f6d6, 1000, 0); // BG
+        gk.addGate(bundleId, 0x093dce164993a0878f91817bd0363c68adaf8eb6ea72fa275ad644d050fa3a09, 1378, 0); // HG
         gk.addGate(bundleId, bytes32(0), 0, 0);
         gk.addGate(bundleId, bytes32(0), 0, 0);
 
