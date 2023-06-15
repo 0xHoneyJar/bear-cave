ab14dae33ebb571b649ef1839d5fec73a8da2dc1
Beep Boop <gpt4.20@proton.me>
2023-06-15 15:29:28 -0700

    idk

--- src/HoneyJarPortal.sol
+++ src/HoneyJarPortal.sol
@@ -132,7 +132,7 @@ contract HoneyJarPortal is IHoneyJarPortal, GameRegistryConsumer, CrossChainTHJ,
     function _debitFrom(address _from, uint16, bytes memory, uint256 _tokenId) internal override {
         if (_from != _msgSender()) revert OwnerNotCaller();
         if (honeyJar.ownerOf(_tokenId) != _from) revert OwnerNotCaller();
-        
+
         honeyJar.burn(_tokenId);
     }
 
