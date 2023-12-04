0655f3db78b061acd385045443e13a5902cda75c
Beep Boop <gpt4.20@proton.me>
2023-12-03 18:26:43 -0800

    adding API3 -- TODO: tests

--- src/VRFProvider.sol
+++ src/VRFProvider.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.19;
 
 import {VRFConsumerBaseV2} from "@chainlink/VRFConsumerBaseV2.sol";
 import {VRFCoordinatorV2Interface} from "@chainlink/interfaces/VRFCoordinatorV2Interface.sol";
+import {RrpRequesterV0} from "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title VRF Provider
 /// @notice Custom VRF Provider that implements the Chainlink provider interface to delegate to another provider.
@@ -10,31 +12,106 @@ import {VRFCoordinatorV2Interface} from "@chainlink/interfaces/VRFCoordinatorV2I
 /// @dev For chains with VRF, use the VRFCoordinator directly
 /// @dev 1 provider for 1 recipient
 /// @dev Future Optmizations: define as abstract contract that is implmented per provider
-contract VRFProvider is VRFCoordinatorV2Interface {
+/// @dev Example: https://github.com/api3dao/qrng-example
+contract VRFProvider is VRFCoordinatorV2Interface, RrpRequesterV0, Ownable {
+    /// Errors
     error NotImplemented();
 
+    /// Events
+    event RequestedUint256Array(bytes32 indexed requestId, uint256 size);
+    event ReceivedUint256Array(bytes32 indexed requestId, uint256[] response);
+
+    /// @notice The recipient of the RNG request that expects a chainlink interface
     VRFConsumerBaseV2 public recipient;
 
-    constructor(VRFConsumerBaseV2 _recipient) {
+    // These variables can also be declared as `constant`/`immutable`.
+    // However, this would mean that they would not be updatable.
+    // Since it is impossible to ensure that a particular Airnode will be
+    // indefinitely available, you are recommended to always implement a way
+    // to update these parameters.
+    address public airnode;
+    bytes32 public endpointIdUint256;
+    bytes32 public endpointIdUint256Array;
+    address public sponsorWallet;
+
+    uint256 public requestNumber;
+    mapping(bytes32 => bool) public expectingRequestWithIdToBeFulfilled;
+    mapping(bytes32 => uint256) public requestIdToRequestNumber;
+
+    /// @dev RrpRequester sponsors itself, meaning that it can make requests
+    /// that will be fulfilled by its sponsor wallet. See the Airnode protocol
+    /// docs about sponsorship for more information.
+    /// @param _provider Airnode RRP contract address
+    constructor(VRFConsumerBaseV2 _recipient, address _provider) RrpRequesterV0(_provider) {
         recipient = _recipient;
-        // TODO: Include relevant Provider configuration within constructor
+    }
+
+    /// @notice Sets parameters used in requesting QRNG services
+    /// @param _airnode Airnode address
+    /// @param _endpointIdUint256Array Endpoint ID used to request a `uint256[]`
+    /// @param _sponsorWallet Sponsor wallet address
+    function setRequestParameters(address _airnode, bytes32 _endpointIdUint256Array, address _sponsorWallet)
+        external
+        onlyOwner
+    {
+        airnode = _airnode;
+        endpointIdUint256Array = _endpointIdUint256Array;
+        sponsorWallet = _sponsorWallet;
+    }
+
+    /// @notice Requests a `uint256[]`
+    /// @dev This request will be fulfilled by the contract's sponsor wallet,
+    /// which means spamming it may drain the sponsor wallet. Implement
+    /// necessary requirements to prevent this, e.g., you can require the user
+    /// to pitch in by sending some ETH to the sponsor wallet, you can have
+    /// the user use their own sponsor wallet, you can rate-limit users.
+    /// @param size Size of the requested array
+    function _makeRequestUint256Array(uint256 size) internal returns (bytes32) {
+        bytes32 requestId = airnodeRrp.makeFullRequest(
+            airnode,
+            endpointIdUint256Array,
+            address(this),
+            sponsorWallet,
+            address(this),
+            this.fulfillUint256Array.selector,
+            // Using Airnode ABI to encode the parameters
+            abi.encode(bytes32("1u"), bytes32("size"), size)
+        );
+
+        expectingRequestWithIdToBeFulfilled[requestId] = true;
+        requestIdToRequestNumber[requestId] = requestNumber;
+        requestNumber++;
+
+        emit RequestedUint256Array(requestId, size);
+    }
+
+    /// @notice Called by the Airnode through the AirnodeRrp contract to
+    /// fulfill the request
+    /// @param requestId Request ID
+    /// @param data ABI-encoded response
+    function fulfillUint256Array(bytes32 requestId, bytes calldata data) external onlyAirnodeRrp {
+        require(expectingRequestWithIdToBeFulfilled[requestId], "Request ID not known");
+        expectingRequestWithIdToBeFulfilled[requestId] = false;
+        uint256 requestNumber = requestIdToRequestNumber[requestId];
+
+        uint256[] memory qrngUint256Array = abi.decode(data, (uint256[]));
+
+        _sendRandomWords(requestNumber, qrngUint256Array);
+
+        emit ReceivedUint256Array(requestId, qrngUint256Array);
     }
 
     /// @inheritdoc VRFCoordinatorV2Interface
     function requestRandomWords(
-        bytes32 keyHash,
-        uint64 subId,
-        uint16 minimumRequestConfirmations,
-        uint32 callbackGasLimit,
+        bytes32, // keyhash
+        uint64, // subId
+        uint16, // minRequestConfirmations
+        uint32, // callbackGasLimit
         uint32 numWords
     ) external returns (uint256 requestId) {
-        // TODO: implment requesting VRF based on provider spec.
-    }
+        require(msg.sender == address(recipient), "Must be configured recipient");
 
-    /// @notice should be permissioned to only work with the VRF provider
-    /// TODO: params
-    function getRandomWords() external {
-        // TODO: external method that is called by the VRF provider with random numbers.
+        _makeRequestUint256Array(numWords);
     }
 
     /// @dev rawFulfillRandomWords valdiates that the reciving contract is being called by a valid VRF Coordinator
